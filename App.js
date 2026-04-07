c2b5bf9726dac3d74991c9eef7fc289214fd3439
simocompsci <hamids9alo7777@gmail.com>
2026-04-07 16:16:18 +0100

    first commit

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <>
-    <View style={{flex:1,}}> 
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-
-      <View style={styles.container}>
+    <View> 
+     
+      <Pressable>
+        <View style={styles.container}>
         <Text>My first time working with react native !</Text>
       </View>
 
       <View style={styles.container}>
         <Text>I will be building a habit tracker app with it !!</Text>
       </View>
+      </Pressable>
     </View>
     </>
   );
