4d03907b882ea21a58eca0e6ee917c4083e3c1e6
simocompsci <hamids9alo7777@gmail.com>
2026-04-14 20:31:00 +0100

    continuing with react native learning

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <>
-    <View> 
-     
-      <Pressable>
-        <View style={styles.container}>
+    <View style={{flex:1}}> 
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+
+      <View style={styles.container}>
         <Text>My first time working with react native !</Text>
       </View>
 
       <View style={styles.container}>
         <Text>I will be building a habit tracker app with it !!</Text>
       </View>
-      </Pressable>
     </View>
     </>
   );
